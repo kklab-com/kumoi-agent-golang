b2a73538801af4c538923ff7cc5bbad7db35d0ff
Yeti Sno <yeti@yetiz.org>
2021-09-29 18:35:46 +0800

    update test

--- kumoi/omega_test.go
+++ kumoi/omega_test.go
@@ -15,12 +15,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestOmega(t *testing.T) {
-	appId := os.Getenv("TEST_APP_ID")
-	token := os.Getenv("TEST_TOKEN")
-	domain := os.Getenv("TEST_DOMAIN")
-	conf := base.NewConfig(appId, token)
+var appId = ""
+var token = ""
+var domain = ""
+var conf *base.Config
+
+func TestMain(m *testing.M) {
+	appId = os.Getenv("TEST_APP_ID")
+	token = os.Getenv("TEST_TOKEN")
+	domain = os.Getenv("TEST_DOMAIN")
+	conf = base.NewConfig(appId, token)
 	conf.Domain = domain
+	m.Run()
+}
+
+func TestOmega(t *testing.T) {
 	o := NewOmegaBuilder(conf).Connect().Omega()
 	assert.NotEmpty(t, o)
 	assert.NotEmpty(t, o.Hello())
@@ -92,70 +101,7 @@ func TestOmega(t *testing.T) {
 	assert.True(t, o.Close().Await().IsSuccess())
 }
 
-func TestOmegaVote(t *testing.T) {
-	appId := os.Getenv("TEST_APP_ID")
-	token := os.Getenv("TEST_TOKEN")
-	domain := os.Getenv("TEST_DOMAIN")
-	conf := base.NewConfig(appId, token)
-	conf.Domain = domain
-	o := NewOmegaBuilder(conf).Connect().Omega()
-	assert.NotEmpty(t, o)
-	assert.NotEmpty(t, o.Hello())
-	assert.NotEmpty(t, o.Time())
-	assert.True(t, o.Broadcast("golang broadcast test"))
-	vtResp := o.CreateVote(apirequest.CreateVote{
-		Name:              "!!!",
-		VoteOptions:       []apirequest.CreateVoteOption{{"vto1"}, {"vto2"}},
-		IdleTimeoutSecond: 300,
-	})
-
-	assert.NotNil(t, vtResp)
-	vtInfo := o.GetVote(vtResp.VoteId)
-	assert.NotNil(t, vtInfo)
-	vt := vtInfo.Join("")
-	vt.OnLeave(func() {
-		println("first leave")
-	})
-
-	vt.Watch(func(msg messages.VoteFrame) {
-		print("w")
-	})
-
-	assert.NotNil(t, vt)
-	assert.True(t, vt.SendMessage("SendMessage"))
-	assert.False(t, vt.SendOwnerMessage("SendOwnerMessage"))
-	assert.False(t, vt.Status(omega.Vote_StatusDeny))
-	assert.True(t, vt.Select(vt.Info().VoteOptions()[1].Id))
-	assert.False(t, vt.Close())
-	assert.Nil(t, vtInfo.Join(""))
-	assert.True(t, vt.Leave())
-
-	vt = vtInfo.Join(vtResp.Key)
-	vt.OnClose(func() {
-		println("first close")
-	})
-
-	vt.Watch(func(msg messages.VoteFrame) {
-		print("w")
-	})
-
-	assert.NotNil(t, vt)
-	assert.True(t, vt.SendOwnerMessage("SendOwnerMessage"))
-	assert.True(t, vt.SetName("new_vote_name"))
-	assert.True(t, vt.Info().VoteOptions()[0].Select())
-	assert.True(t, vt.Status(omega.Vote_StatusDeny))
-	assert.False(t, vt.Select(vt.Info().VoteOptions()[1].Id))
-	assert.Equal(t, int32(1), vt.GetCount().GetVoteCount().VoteOptions[0].Count)
-	assert.True(t, vt.Close())
-	assert.True(t, o.Close().Await().IsSuccess())
-}
-
 func TestOmegaDisconnect(t *testing.T) {
-	appId := os.Getenv("TEST_APP_ID")
-	token := os.Getenv("TEST_TOKEN")
-	domain := os.Getenv("TEST_DOMAIN")
-	conf := base.NewConfig(appId, token)
-	conf.Domain = domain
 	o := NewOmegaBuilder(conf).Connect().Omega()
 	bwg := concurrent.BurstWaitGroup{}
 	go func() {
@@ -185,11 +131,6 @@ func TestOmegaDisconnect(t *testing.T) {
 }
 
 func TestOmegaWriteOnClosed(t *testing.T) {
-	appId := os.Getenv("TEST_APP_ID")
-	token := os.Getenv("TEST_TOKEN")
-	domain := os.Getenv("TEST_DOMAIN")
-	conf := base.NewConfig(appId, token)
-	conf.Domain = domain
 	o := NewOmegaBuilder(conf).Connect().Omega()
 	chResp := o.CreateChannel(apirequest.CreateChannel{})
 	chInfo := o.GetChannel(chResp.ChannelId)
@@ -200,14 +141,9 @@ func TestOmegaWriteOnClosed(t *testing.T) {
 }
 
 func TestOmegaKeepAlive(t *testing.T) {
-	appId := os.Getenv("TEST_APP_ID")
-	token := os.Getenv("TEST_TOKEN")
-	domain := os.Getenv("TEST_DOMAIN")
-	conf := base.NewConfig(appId, token)
-	conf.Domain = domain
 	o := NewOmegaBuilder(conf).Connect().Omega()
 	<-time.After(50 * time.Second)
 	assert.False(t, o.IsClosed())
 	assert.False(t, o.IsDisconnected())
-	o.Close().Await()
+	assert.True(t, o.Close().Await().IsSuccess())
 }
---
+++ kumoi/vote_test.go
@@ -0,0 +1,92 @@
+package kumoi
+
+import (
+	"testing"
+	"time"
+
+	"github.com/kklab-com/kumoi-agent-golang/base/apirequest"
+	"github.com/kklab-com/kumoi-agent-golang/kumoi/messages"
+	omega "github.com/kklab-com/kumoi-protobuf-golang"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOmegaVote(t *testing.T) {
+	o := NewOmegaBuilder(conf).Connect().Omega()
+	assert.NotEmpty(t, o)
+	assert.NotEmpty(t, o.Hello())
+	assert.NotEmpty(t, o.Time())
+	assert.True(t, o.Broadcast("golang broadcast test"))
+	vtResp := o.CreateVote(apirequest.CreateVote{
+		Name:              "!!!",
+		VoteOptions:       []apirequest.CreateVoteOption{{"vto1"}, {"vto2"}},
+		IdleTimeoutSecond: 300,
+	})
+
+	assert.NotNil(t, vtResp)
+	vtInfo := o.GetVote(vtResp.VoteId)
+	assert.NotNil(t, vtInfo)
+	vt := vtInfo.Join("")
+	vt.OnLeave(func() {
+		println("first leave")
+	})
+
+	vt.Watch(func(msg messages.VoteFrame) {
+		print("w")
+	})
+
+	assert.NotNil(t, vt)
+	assert.True(t, vt.SendMessage("SendMessage"))
+	assert.False(t, vt.SendOwnerMessage("SendOwnerMessage"))
+	assert.False(t, vt.Status(omega.Vote_StatusDeny))
+	assert.True(t, vt.Select(vt.Info().VoteOptions()[1].Id))
+	assert.False(t, vt.Close())
+	assert.Nil(t, vtInfo.Join(""))
+	assert.True(t, vt.Leave())
+
+	vt = vtInfo.Join(vtResp.Key)
+	vt.OnClose(func() {
+		println("first close")
+	})
+
+	vt.Watch(func(msg messages.VoteFrame) {
+		print("w")
+	})
+
+	assert.NotNil(t, vt)
+	assert.True(t, vt.SendOwnerMessage("SendOwnerMessage"))
+	assert.True(t, vt.SetName("new_vote_name"))
+	assert.True(t, vt.Info().VoteOptions()[0].Select())
+	assert.True(t, vt.Status(omega.Vote_StatusDeny))
+	assert.False(t, vt.Select(vt.Info().VoteOptions()[1].Id))
+	assert.Equal(t, int32(1), vt.GetCount().GetVoteCount().VoteOptions[0].Count)
+	assert.True(t, vt.Close())
+	assert.True(t, o.Close().Await().IsSuccess())
+}
+
+func TestOmegaVoteWatch(t *testing.T) {
+	o := NewOmegaBuilder(conf).Connect().Omega()
+	assert.NotEmpty(t, o)
+	vtResp := o.CreateVote(apirequest.CreateVote{
+		Name:              "!!!",
+		VoteOptions:       []apirequest.CreateVoteOption{{"vto1"}, {"vto2"}},
+		IdleTimeoutSecond: 300,
+	})
+
+	assert.NotNil(t, vtResp)
+	vtInfo := o.GetVote(vtResp.VoteId)
+	assert.NotNil(t, vtInfo)
+	vt := vtInfo.Join(vtResp.Key)
+	vmCount := 0
+	vt.Watch(func(msg messages.VoteFrame) {
+		if _, ok := msg.(*messages.VoteMessage); ok {
+			vmCount++
+		}
+	})
+
+	assert.NotNil(t, vt)
+	assert.True(t, vt.SendMessage("SendMessage"))
+	time.Sleep(time.Second)
+	assert.Equal(t, 1, vmCount)
+	assert.True(t, vt.Close())
+	assert.True(t, o.Close().Await().IsSuccess())
+}
